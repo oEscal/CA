11a870a0bdeb1474331b8771b4f62a398b5f0cd9
escaleira <pedroescaleira@hotmail.com>
2020-10-30 09:46:31 +0000

    hashes -> birthday paradox and minor fixes

--- pratical/1_symetric_crypto/utils.py
+++ pratical/1_symetric_crypto/utils.py
@@ -1,19 +1,19 @@
-def save_bin_file(file_name: str, content: bin):
+def save_bytes_file(file_name: str, content: bytes):
    with open(file_name, 'wb') as file:
       file.write(content)
 
 
-def open_bin_file(file_name: str) -> bin:
+def open_bytes_file(file_name: str) -> bytes:
    with open(file_name, 'rb') as file:
       return file.read()
 
 
-def read_bin_file_one_chunk(file_name: str, chunk_size: int):
+def read_bytes_file_one_chunk(file_name: str, chunk_size: int):
    with open(file_name, 'rb') as file:
       return file.read(chunk_size)
 
 
-def read_bin_file_in_chunks(file_name: str, chunk_size: int, starting_bit: int = 0) -> bin:
+def read_bytes_file_in_chunks(file_name: str, chunk_size: int, starting_bit: int = 0) -> bytes:
    with open(file_name, 'rb') as file:
       file.seek(starting_bit, 0)
       while True:
